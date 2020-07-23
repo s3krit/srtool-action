5d754331609a39c2255debc69d0f65793784095a
Martin Pugh <pugh@s3kr.it>
2020-07-23 17:40:30 +0200

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd /builds || 1
-mv "$GITHUB_WORKSPACE/*" .
+# Not actually sure if it just gets made...
+cd /build || exit 1
+mv "$GITHUB_WORKSPACE"/* .
 RUSTC_VERSION=nightly-2020-07-20 PACKAGE=$INPUT_RUNTIME build

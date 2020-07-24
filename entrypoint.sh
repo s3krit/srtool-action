d19daba3d509852688c119b33621e9fa30505496
Martin Pugh <pugh@s3kr.it>
2020-07-24 12:15:47 +0200

    remove build directory population

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Not actually sure if it just gets made...
 cd /build || exit 1
-mv "$GITHUB_WORKSPACE"/* .
-mv "$GITHUB_WORKSPACE"/.* .
 RUSTC_VERSION=nightly-2020-07-20 PACKAGE=$INPUT_RUNTIME build --json | tee srtool_output.json
 # Set a Github Action output for each key
 while IFS= read -r line; do

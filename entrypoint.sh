476cccafef021e5eb23ceb1629371bbd07873fae
Martin Pugh <pugh@s3kr.it>
2020-07-24 12:20:13 +0200

    some debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Not actually sure if it just gets made...
 cd /build || exit 1
+cp -r "$GITHUB_WORKSPACE"/* .
+cp -r "$GITHUB_WORKSPACE"/.* .
+ls /build
+pwd
 RUSTC_VERSION=nightly-2020-07-20 PACKAGE=$INPUT_RUNTIME build --json | tee srtool_output.json
+ls /build
+ls /build/target
+find /build/target
 # Set a Github Action output for each key
 while IFS= read -r line; do
   echo "::set-output name=$line::$(jq -r ".$line" < srtool_output.json)"

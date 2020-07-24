9f5bea5808a4f3a6d9cfeb20226f34f31009e3ef
Martin Pugh <pugh@s3kr.it>
2020-07-24 16:41:17 +0200

    fix again... :(

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,19 @@ cd /build || exit 1
 cp -r "$GITHUB_WORKSPACE"/* /build
 cp -r "$GITHUB_WORKSPACE"/.* /build
 
-# RUSTC_VERSION=nightly-2020-07-20 PACKAGE=$INPUT_RUNTIME build --json | tee srtool_output.json
-cat /build/target/testfile
-mkdir /build/target
-echo "test" > /build/target/testfile
+RUSTC_VERSION=nightly-2020-07-20 PACKAGE=$INPUT_RUNTIME build --json | tee srtool_output.json
+
 # copy the target dir back to preserve it
 cp -r /build/target "$GITHUB_WORKSPACE"
 
+du -hs "$GITHUB_WORKSPACE"
+
 # Set a Github Action output for each key
-#while IFS= read -r line; do
-#  echo "::set-output name=$line::$(jq -r ".$line" < srtool_output.json)"
-#done <<< "$(jq -r 'keys[]' < srtool_output.json)"
+while IFS= read -r line; do
+  echo "::set-output name=$line::$(jq -r ".$line" < srtool_output.json)"
+done <<< "$(jq -r 'keys[]' < srtool_output.json)"
 
 # Copy the runtime from /builds to GITHUB_WORKSPACE to get around this weird artifact bug
 cp "$(jq -r '.wasm' < srtool_output.json)" "$GITHUB_WORKSPACE"
 path="$(jq -r '.wasm' < srtool_output.json | sed "s_^\._${GITHUB_WORKSPACE}_")"
-echo "::set-output name=wasm::/build/target/testfile"
+echo "::set-output name=wasm::$path"
